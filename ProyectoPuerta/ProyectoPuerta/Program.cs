97a343ed864690bc741d2587780b1c444a2b86ea
xalberty <xalberty24@gmail.com>
2022-03-26 13:42:43 +0100

    ProyectoPuerta

---
+++ ProyectoPuerta/ProyectoPuerta/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPuerta
+{
+    class Program
+    {
+		static Puerta door = new Puerta(100, 80);
+
+		static void Main(string[] args)
+        {
+			
+			int opcion = 0, cont = 0;
+
+            do
+            {
+				if(cont > 0)
+                {
+                    Console.Write("\n\n\n\t\t\t\t\tEnter para limpiar la pantalla ");
+					Console.ReadLine();
+					Console.Clear();
+                }
+				cont++;
+
+				opcion = Tools.Menu();
+
+				switch(opcion)
+                {
+					case 1:
+						door.Abrir();
+						break;
+					case 2:
+						door.Cerrar();
+						break;
+					case 3:
+						door.Mostrar();
+						break;
+					case 4:
+						door.Color = Tools.EligeColor();
+                        Console.WriteLine("\n\t\t\t\t\tEl color se ha cambiado");
+						break;
+					case 5:
+						ModifyDoor();
+						break;
+					case 0:
+                        Console.WriteLine("\n\n\t\t\t\t\tThx for using our app");
+						break;
+
+				}
+
+
+            }while (opcion != 0);
+
+            Console.Write("\n\n\n\n\t\t\t\t\tPress enter to skip ");
+            Console.ReadLine();
+		}
+
+        static Puerta ModifyDoor() 
+        {
+            Console.WriteLine("\n\n\t----- Construyamos la puerta ------");
+            int alto = Tools.CapturaEntero("\n\t¿Altura en cm?", 50, 250);
+            int ancho = Tools.CapturaEntero("\n\t¿Anchura en cm?", 30, 250);
+			ConsoleColor color = Tools.EligeColor();
+
+			door.Alto = alto;
+			door.Ancho = ancho;
+			door.Color = color;
+
+			return door;
+        }
+
+    }
+}
---
+++ ProyectoPuerta/ProyectoPuerta/Puerta.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPuerta
+{
+    class Puerta
+    {
+        int alto;
+        int ancho;
+        ConsoleColor color;
+        bool estado = false; //** Yo he controlado el estado así pero, si quieres puedes hacerlo de otro modo 
+
+
+        #region Propiedades
+        public int Alto { get => alto; set => alto = value; }
+        public int Ancho { get => ancho; set => ancho = value; }
+        public ConsoleColor Color { get => color; set => color = value; }
+        public bool Estado { get => estado; set => estado = value; }
+
+        #endregion
+
+
+        //---- Constructores -----
+
+        public Puerta(int alto, int ancho)
+        {
+            this.alto = alto;
+            this.ancho = ancho;
+            color = ConsoleColor.White;
+        }
+
+        public Puerta(int alto, int ancho, ConsoleColor color)
+        {
+            this.alto = alto;
+            this.ancho = ancho;
+            this.color = color;
+        }
+
+
+        //---- Métodos ----
+        public void Abrir()
+        {
+            if (estado)
+                Console.WriteLine("\n\n\t\t\t\t\tLa puerta ya estaba abierta!");
+            else
+            {
+                Console.WriteLine("\n\n\t\t\t\t\tLa puera ahora está abierta!");
+                estado = true;
+            }
+        }
+
+        public void Cerrar()
+        {
+            if (!estado)
+                Console.WriteLine("\n\n\t\t\t\t\tLa puerta ya estaba cerrada!");
+            else
+            {
+                Console.WriteLine("\n\n\t\t\t\t\tLa puera ahora está cerrada!");
+                estado = false;
+            }
+        }
+
+        public void Mostrar()
+        {
+            if (!estado)
+                Console.WriteLine("\n\t\t\t\t\tEstado: Cerrada");
+            else
+                Console.WriteLine("\n\t\t\t\t\tEstado: Abierta");
+            Console.WriteLine("\n\t\t\t\t\tAlto: {0}", alto);
+            Console.WriteLine("\n\t\t\t\t\tAncho: {0}", ancho);
+
+            Console.Write("\n\t\t\t\t\tColor: ");
+            Console.ForegroundColor = color;
+            Console.Write(" ████████ ");
+            Console.ResetColor();
+            Console.WriteLine("◄ Este color");
+            Console.ResetColor();
+        }
+    }
+}
